d4c2b3e3d13d7a7cd38449bf7f84e74853d53ef3
Kwak <brettkwak@gmail.com>
2025-08-30 03:37:05 +0900

    feat: Add Flashcard

---
+++ src/main/java/com/flashcards/model/Flashcard.java
@@ -0,0 +1,63 @@
+package com.flashcards.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Flashcard {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String front;
+
+    @Column(nullable = false, length = 1000)
+    private String back;
+
+    private Set<Tag> tags = new HashSet<>();
+
+    // Constructor
+    public Flashcard() {}
+
+    public Flashcard(String front, String back) {
+        this.front = front;
+        this.back = back;
+    }
+
+    // Gets, Sets
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public void setFront(String front) {
+        this.front = front;
+    }
+
+    public String getBack() {
+        return back;
+    }
+
+    public void setBack(String back) {
+        this.back = back;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+
+}
