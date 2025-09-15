2532898d7a945367bfe2d5e145014c8a091a8c25
BKwak28 <brettkwak@gmail.com>
2025-09-16 04:20:46 +0900

    feat : Add FlashcardSet entity

---
+++ src/main/java/com/flashcards/model/FlashcardSet.java
@@ -0,0 +1,30 @@
+package com.flashcards.model;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlashcardSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+
+    @OneToMany(mappedBy = "flashcardSet", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Flashcard> flashcards = new ArrayList<>();
+
+
+    // Gets, Sets
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle() { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public List<Flashcard> getFlashcards() { return flashcards; }
+    public void setFlashcards(List<Flashcard> flashcards) { this.flashcards = flashcards; }
+
+
+}
