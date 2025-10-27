373e9dfef791d1b4cb54e9764e8d60684036e0be
BKwak28 <brettkwak@gmail.com>
2025-10-27 15:15:02 +0900

    feat : Display list of sets on main page

--- src/main/java/com/flashcards/controller/HomeController.java
+++ src/main/java/com/flashcards/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.flashcards.controller;
 
+import com.flashcards.model.FlashcardSet;
+import com.flashcards.repository.FlashcardSetRepository;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final FlashcardSetRepository flashcardSetRepository;
+
+    public HomeController(FlashcardSetRepository flashcardSetRepository) {
+        this.flashcardSetRepository = flashcardSetRepository;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<FlashcardSet> sets = flashcardSetRepository.findAll();
+        model.addAttribute("flashcardSets", sets);
         return "index";
     }
 
