abe310f51f82f2f1a4baea67e4b4eef964effc21
BKwak28 <brettkwak@gmail.com>
2026-01-06 16:51:43 +0900

    feat : Create DTO for Flashcard

--- src/main/java/com/flashcards/dto/FlashcardDTO.java
+++ src/main/java/com/flashcards/dto/FlashcardDTO.java
@@ -1,4 +1,19 @@
 package com.flashcards.dto;
 
 public class FlashcardDTO {
+    public String front;
+    public String back;
+
+    // Constructor
+    public FlashcardDTO(String front, String back) {
+        this.front = front;
+        this.back = back;
+    }
+
+    // Gets, Sets
+    public String getFront() { return front; }
+    public void setFront(String front) { this.front = front; }
+    public String getBack() { return back; }
+    public void getBack(String back) { this.back = back; }
+
 }
