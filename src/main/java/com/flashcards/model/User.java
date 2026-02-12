2c1d36ea9231addca5ba866ce1632e69e45dae66
BKwak28 <brettkwak@gmail.com>
2026-02-12 20:09:02 +0900

    feat : Add JPA annotations for User

--- src/main/java/com/flashcards/model/User.java
+++ src/main/java/com/flashcards/model/User.java
@@ -1,9 +1,22 @@
 package com.flashcards.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String role;
 
     // Constructor
