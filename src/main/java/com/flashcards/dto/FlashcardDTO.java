4fd5e6175cb852b85874052c14337069af1e9676
BKwak28 <brettkwak@gmail.com>
2026-02-12 11:40:14 +0900

    fix : Fix typo

--- src/main/java/com/flashcards/dto/FlashcardDTO.java
+++ src/main/java/com/flashcards/dto/FlashcardDTO.java
@@ -21,6 +21,6 @@ public class FlashcardDTO {
     public String getFront() { return front; }
     public void setFront(String front) { this.front = front; }
     public String getBack() { return back; }
-    public void getBack(String back) { this.back = back; }
+    public void setBack(String back) { this.back = back; }
 
 }
