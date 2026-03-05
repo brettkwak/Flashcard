23215fa51c28af5413ed8bed90625d31ee6dac63
BKwak28 <brettkwak@gmail.com>
2026-03-05 23:55:59 +0900

    feat : Add findByUser to FlashcardSetRepository

--- src/main/java/com/flashcards/repository/FlashcardSetRepository.java
+++ src/main/java/com/flashcards/repository/FlashcardSetRepository.java
@@ -1,4 +1,8 @@
 package com.flashcards.repository;
 import com.flashcards.model.FlashcardSet;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface FlashcardSetRepository extends JpaRepository<FlashcardSet, Long> {}
+public interface FlashcardSetRepository extends JpaRepository<FlashcardSet, Long> {
+
+    List<FlashcardSet> findByUser(User user);
+
+}
