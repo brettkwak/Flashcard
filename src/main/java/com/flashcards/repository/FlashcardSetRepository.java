5d2e2d8467b3ecd7ca62bc0b7c5295ceb6f267a2
BKwak28 <brettkwak@gmail.com>
2026-03-09 09:55:02 +0900

    fix : Fix imports of FlashcardSetRepository

--- src/main/java/com/flashcards/repository/FlashcardSetRepository.java
+++ src/main/java/com/flashcards/repository/FlashcardSetRepository.java
@@ -1,5 +1,7 @@
 package com.flashcards.repository;
 import com.flashcards.model.FlashcardSet;
+import com.flashcards.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface FlashcardSetRepository extends JpaRepository<FlashcardSet, Long> {
 
